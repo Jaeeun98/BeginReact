70bdc17f65be3ad433a9ba353170c0dd6edba283
Jaeeun98 <wodms4525@naver.com>
2021-01-18 22:08:17 +0900

    Title : Add
    배열에 항목 추가하기

--- count-app/src/App.js
+++ count-app/src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './App.css';
 import Count from './Count';
+import CreateUser from './CreateUser';
 import Input from './Input'
 import UseRefTest from './UseRefTest';
 
 function App() {
+  const [input, setInput] = useState({
+    userName:'',
+    email:''
+  })
+  const {userName, email} = input;
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInput({
+      ...input,
+      [name] : value
+    })
+  }
+  const [users, setUsers] = useState([
+    {
+        id : 1,
+        userName : "jaeeun",
+        email : "jaeeun@naver.com"
+    },
+    {
+        id : 2,
+        userName : "jineun",
+        email : "jineun@naver.com"
+    },
+    {
+        id : 3,
+        userName : "dubu",
+        email : "jdubu@naver.com"
+    }
+])
+const nextId = useRef(4);
+const onCreate = () => {
+  const user = {
+    id : nextId.current,
+    userName,
+    email
+  }
+
+  setUsers([...users, user]);
+
+  setInput({
+    userName : '',
+    email:'',
+  })
+  nextId.current += 1;
+}
   return (
     <div className="App">
       <header>
@@ -13,8 +59,9 @@ function App() {
       </header>
 
       <Count />
-
-      <UseRefTest />
+      <CreateUser userName={userName} email={email} onChange={onChange} onCreate={onCreate} />
+      <UseRefTest users={users} />
+      
     </div>
   );
 }
--- count-app/src/UseRefTest.js
+++ count-app/src/UseRefTest.js
@@ -1,33 +1,16 @@
 import React, {useRef} from 'react';  //useRef 가져오기
 
 
-function UseRefTest(){
+function UseRefTest({users}){  //props 비구조화 할당
     const use = useRef();
-    const users = [
-        {
-            id : 1,
-            name : "jaeeun",
-            age : 25,
-        },
-        {
-            id : 2,
-            name : "jineun",
-            age : 22,
-        },
-        {
-            id : 3,
-            name : "dubu",
-            age : 7,
-        }
-    ]
     const handleClick = () => use.current.focus();
     return (
         <div>
             {users.map(item => (
               <div>
                   <span>{item.id}, </span>
-                  <span>{item.name}, </span>
-                  <span>{item.age}</span>    
+                  <span>{item.userName}, </span>
+                  <span>{item.email}</span>    
               </div>
             ))}
             <button onClick={handleClick}>F</button>
